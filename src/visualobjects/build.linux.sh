4a1ef37a676f9681af438579735cc532016631c9
Sridhar Madhugiri <sridmad@microsoft.com>
2018-11-24 16:42:13 -0800

    Update Linux build script

--- src/visualobjects/build.linux.sh
+++ src/visualobjects/build.linux.sh
@@ -1,3 +1,19 @@
-docker build . -f web/Dockerfile -t azure-mesh-visualobjects-web:dev-alpine
-docker build . -f worker/Dockerfile -t azure-mesh-visualobjects-worker:dev-alpine
-docker build . -f worker/rotate.Dockerfile -t azure-mesh-visualobjects-worker-rotate:dev-alpine
+#!/bin/sh
+
+../scripts/linux/build_and_publish.sh visualobjects-web.imageinfo.txt "$@" || {
+    echo Failed to process image for visualobjects-web
+    exit 1
+}
+
+../scripts/linux/build_and_publish.sh visualobjects-worker.imageinfo.txt "$@" || {
+    echo Failed to process image for visualobjects-worker
+    exit 1
+}
+
+../scripts/linux/build_and_publish.sh visualobjects-worker-rotate.imageinfo.txt "$@" || {
+    echo Failed to process image for visualobjects-worker-rotate
+    exit 1
+}
+
+echo Successfully build visualobjects images.
+exit 0
